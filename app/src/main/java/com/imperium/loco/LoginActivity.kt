338151a8662c71db4ba35325b38b3d7ac4831374
Baymax15 <39257699+Baymax15@users.noreply.github.com>
2020-10-17 16:54:50 +0530

    First Page
    Edit manifest, add splash screen
    Add LoginActivity
    Add SplashActivity
    Add/Edit resources

---
+++ app/src/main/java/com/imperium/loco/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.imperium.loco
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun requestLogin(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun requestRegister(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
